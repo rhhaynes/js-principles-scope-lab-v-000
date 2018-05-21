81747dc62e7699638d7b0bbb62cf82ad6ed9c269
Randall Harris Haynes <harrishaynes@gmail.com>
2018-05-21 00:17:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var customerName = 'bob';
 
-function upperCaseCustomerName(customerName){
-  return customerName.toUpperCase();
+function upperCaseCustomerName(str){
+  return str.toUpperCase();
 }
 
 function setBestCustomer(){
